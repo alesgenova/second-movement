f5fb6eb843ace89ff0b0aab0089113009b81a2bd
David Volovskiy <devolov@gmail.com>
2025-10-19 12:16:15 -0400

    Included return value of I2C calls

--- watch-library/hardware/watch/watch_i2c.c
+++ watch-library/hardware/watch/watch_i2c.c
@@ -23,7 +23,6 @@
  */
 
 #include "watch_i2c.h"
-#include "i2c.h"
 
 #ifdef I2C_SERCOM
 
@@ -38,29 +37,29 @@ void watch_disable_i2c(void) {
     i2c_disable();
 }
 
-int8_t watch_i2c_send(int16_t addr, uint8_t *buf, uint16_t length) {
-    return (int8_t)i2c_write(addr, buf, length);
+i2c_result_t watch_i2c_send(int16_t addr, uint8_t *buf, uint16_t length) {
+    return i2c_write(addr, buf, length);
 }
 
-int8_t watch_i2c_receive(int16_t addr, uint8_t *buf, uint16_t length) {
-    return (int8_t)i2c_read(addr, buf, length);
+i2c_result_t watch_i2c_receive(int16_t addr, uint8_t *buf, uint16_t length) {
+    return i2c_read(addr, buf, length);
 }
 
-int8_t watch_i2c_write8(int16_t addr, uint8_t reg, uint8_t data) {
+i2c_result_t watch_i2c_write8(int16_t addr, uint8_t reg, uint8_t data) {
     uint8_t buf[2];
     buf[0] = reg;
     buf[1] = data;
 
-    return (int8_t)watch_i2c_send(addr, (uint8_t *)&buf, 2);
+    return watch_i2c_send(addr, (uint8_t *)&buf, 2);
 }
 
 uint8_t watch_i2c_read8(int16_t addr, uint8_t reg) {
     uint8_t data;
 
-    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != 0) {
+    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != I2C_RESULT_SUCCESS) {
         return 0;
     }
-    if (watch_i2c_receive(addr, (uint8_t *)&data, 1) != 0) {
+    if (watch_i2c_receive(addr, (uint8_t *)&data, 1) != I2C_RESULT_SUCCESS) {
         return 0;
     }
 
@@ -70,10 +69,10 @@ uint8_t watch_i2c_read8(int16_t addr, uint8_t reg) {
 uint16_t watch_i2c_read16(int16_t addr, uint8_t reg) {
     uint16_t data;
 
-    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != 0) {
+    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != I2C_RESULT_SUCCESS) {
         return 0;
     }
-    if (watch_i2c_receive(addr, (uint8_t *)&data, 2) != 0) {
+    if (watch_i2c_receive(addr, (uint8_t *)&data, 2) != I2C_RESULT_SUCCESS) {
         return 0;
     }
     return data;
@@ -83,10 +82,10 @@ uint32_t watch_i2c_read24(int16_t addr, uint8_t reg) {
     uint32_t data;
     data = 0;
 
-    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != 0) {
+    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != I2C_RESULT_SUCCESS) {
         return 0;
     }
-    if (watch_i2c_receive(addr, (uint8_t *)&data, 3) != 0) {
+    if (watch_i2c_receive(addr, (uint8_t *)&data, 3) != I2C_RESULT_SUCCESS) {
         return 0;
     }
     return data << 8;
@@ -95,10 +94,10 @@ uint32_t watch_i2c_read24(int16_t addr, uint8_t reg) {
 uint32_t watch_i2c_read32(int16_t addr, uint8_t reg) {
     uint32_t data;
 
-    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != 0) {
+    if (watch_i2c_send(addr, (uint8_t *)&reg, 1) != I2C_RESULT_SUCCESS) {
         return 0;
     }
-    if (watch_i2c_receive(addr, (uint8_t *)&data, 4) != 0) {
+    if (watch_i2c_receive(addr, (uint8_t *)&data, 4) != I2C_RESULT_SUCCESS) {
         return 0;
     }
     return data;
