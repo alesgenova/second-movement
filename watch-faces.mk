38869c7d59f8fa46c3425e70ff9a84b23bad01da
Alessandro Genova <ales.genova@gmail.com>
2025-11-24 20:31:30 -0500

    Merge and squash the step-counter branch

--- watch-faces.mk
+++ watch-faces.mk
@@ -58,4 +58,5 @@ SRCS += \
   ./watch-faces/complication/lander_face.c \
   ./watch-faces/complication/simon_face.c \
   ./watch-faces/settings/tunes_face.c \
+  ./watch-faces/complication/step_counter_face.c \
 # New watch faces go above this line.
